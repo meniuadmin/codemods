70b58024ea510679de72ecb436c7b4cdbf28a576
ivanunderwood <meniuapp@gmail.com>
2023-04-18 22:30:52 -0600

    v2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Mrm module to work with new line separated text files
 const { lines } = require("mrm-core");
+const { template } = require("mrm-core");
 
 module.exports = function task() {
   // Read .gitignore if it exists
@@ -7,6 +8,13 @@ module.exports = function task() {
     // Add lines that do not exist in a file yet,
     // but keep all existing lines
     .add(["node_modules/", ".DS_Store"])
+    .add(["teeeest"])
     // Update or create a file
+    .save();
+
+  template("eslint-config-js", path.join(__dirname, "eslint-config-js"))
+    .apply(config(), {
+      year: new Date().getFullYear(),
+    })
     .save();
 };
